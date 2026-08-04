d3a9357568d59ec1af18a9cdc3b050a0b2560fb7
schoolofhowtolearn <90174141+schoolofhowtolearn@users.noreply.github.com>
2026-08-04 14:58:24 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// ==========================================================
+// Firebase の設定情報をここに入力してください。
+// Firebaseコンソール > プロジェクトの設定 > マイアプリ > SDK の設定と構成
+// からコピーできます。
+// ==========================================================
+const firebaseConfig = {
+  apiKey: "AIzaSyABmK5aGVAdcyL6Ei9nogUp-NOF-BxSOLI",
+  authDomain: "posting-tracker-99ebb.firebaseapp.com",
+  projectId: "posting-tracker-99ebb",
+  storageBucket: "posting-tracker-99ebb.firebasestorage.app",
+  messagingSenderId: "905270977413",
+  appId: "1:905270977413:web:664155c75a0451b3711aec"
+};
